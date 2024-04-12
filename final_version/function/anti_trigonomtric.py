b1db55dc5836aaa121b9fa0c88a516749e2cbd2d
zhoushiyao <120156925+2530928561@users.noreply.github.com>
2024-04-12 16:38:37 +0800

    Add files via upload

---
+++ final_version/function/anti_trigonomtric.py
@@ -0,0 +1,21 @@
+from function.factorial import factorial
+from function.power import power
+
+PI=3.1415926535897932384626433832795028841971
+
+n = 10
+
+def my_arcsin(x):
+    result = 0.0
+    for i in range(n + 1):
+        result += (factorial(2 * i) / (power(2, 2 * i) * factorial(i) * factorial(i) * (2 * i + 1))) * power(x, 2 * i + 1)
+    return result
+
+def my_arccos(x):
+    return PI / 2 - my_arcsin(x)
+
+def my_arctan(x):
+    result = 0.0
+    for i in range(n + 1):
+        result += power(-1, i) * power(x, 2 * i + 1) / (2 * i + 1)
+    return result
