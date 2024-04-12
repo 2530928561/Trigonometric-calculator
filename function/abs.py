fe26d1cb31fdecbf81591a37c66d55e74eb8e491
MichaelDael <144248754+MichaelDael@users.noreply.github.com>
2024-04-12 16:06:00 +0800

    Delete function directory

--- function/abs.py
+++
@@ -1,2 +0,0 @@
-def my_abs(num):
-    return num if num > 0 else -num
--- function/power.py
+++
@@ -1,7 +0,0 @@
-def power(x, n):
-    if n == 0:
-        return 1.0
-    elif n < 0:
-        return 1.0 / power(x, -n)
-    else:
-        return x * power(x, n - 1)
